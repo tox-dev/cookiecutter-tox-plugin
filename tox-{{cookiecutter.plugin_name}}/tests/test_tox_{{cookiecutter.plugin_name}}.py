2ab552f692bc17e144c587541f4d22396cd813f1
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-04-11 17:27:11 +0200

    update todo

--- tox-{{cookiecutter.plugin_name}}/tests/test_tox_{{cookiecutter.plugin_name}}.py
+++ tox-{{cookiecutter.plugin_name}}/tests/test_tox_{{cookiecutter.plugin_name}}.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 # TODO think about how to test this properly
-# TODO check tox own tests to see how to do this
-# TODO (also check, if someone did this already)
-
+# TODO check tox own tests to see how to do this (use of _pytestplugin)
+# TODO (also check if someone did this already)
