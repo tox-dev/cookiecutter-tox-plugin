f1cd28e14e36bae9ce59894d6d271c5005899864
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-04-11 17:01:49 +0200

    adapt validation for package structure

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,23 +6,28 @@ import re
 import sys
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('pre_gen_project')
+log = logging.getLogger('pre_gen_project')
 
-PLUGIN_REGEX = r'^(?!tox)[-_a-zA-Z][-_a-zA-Z0-9]+$'
 
-plugin_name = '{{cookiecutter.plugin_name}}'
+def validate_plugin_name(name):
+    matcher = re.compile(r'^(?!tox)[-_a-zA-Z][-_a-zA-Z0-9]+$')
+    if not re.match(matcher, name):
+        log.error('Invalid value for plugin_name "%s"', name)
+        log.info('Must match %s', matcher.pattern)
+        log.info('Do not prepend plugin_name with "tox"!')
+        sys.exit(1)
 
-if not re.match(PLUGIN_REGEX, plugin_name):
-    logger.error('Invalid value for plugin_name "{}"'.format(plugin_name))
-    logger.debug('Please do not prepend plugin_name with "tox"!')
-    sys.exit(1)
 
-MODULE_REGEX = r'^[a-z][_a-z0-9]+$'
+def validate_py_name(which, name):
+    matcher = re.compile(r'^[a-z][_a-z0-9]+$')
+    if not re.match(matcher, name):
+        url = 'https://python.org/dev/peps/pep-0008/#package-and-module-names'
+        log.error('%s "%s" not PEP-8 compliant - see %s', which, name, url)
+        log.info('Must match %s', matcher.pattern)
+        sys.exit(1)
 
-module_name = '{{cookiecutter.module_name}}'
 
-if not re.match(MODULE_REGEX, module_name):
-    link = 'https://www.python.org/dev/peps/pep-0008/#package-and-module-names'
-    logger.error('Module name should be pep-8 compliant.')
-    logger.error('  More info: {}'.format(link))
-    sys.exit(1)
+if __name__ == '__main__':
+    validate_plugin_name('{{cookiecutter.plugin_name}}')
+    validate_py_name('pkg_name', '{{cookiecutter.pkg_name}}')
+    validate_py_name('module_name', '{{cookiecutter.module_name}}')
